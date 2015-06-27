ccbc3c9acbc3b13ed2b8f175ddfd14cd455470a2
Joe Rowley <jrowley@vertigo.com>
2015-06-26 19:54:05 -0600

    The data binding issues are solved. Not sure it's the best way but it's a way

--- src/ui-winjs/model/fillupModel.js
+++ src/ui-winjs/model/fillupModel.js
@@ -1,17 +1,45 @@
 ﻿WinJS.Namespace.define("Fillup", {
-    Fillup: WinJS.Class.define(function () { },
-        {carId: null,
-        dateLong: null,
-        milesFloat: null,
-        gallonsFloat: null,
-        pricePerGallon: null,
-        totalCost: null,
+    Fillup: WinJS.Class.define(function (payload) {
+        this.carId = payload.carId;
+        this.dateLong = payload.date;
+        this.milesFloat = payload.miles;
+        this.gallonsFloat = payload.gallons;
+        this.pricePerGallon = payload.pricePerGallon;
+        this.totalCost = payload.totalCost;
+        this.displayMileage = this.figureDisplayMileage();
+        this.displayDate = this.figureDisplayDate();
+    },
+        {
+            carId: null,
+            dateLong: null,
+            milesFloat: null,
+            gallonsFloat: null,
+            pricePerGallon: null,
+            totalCost: null,
+            displayMileage: null,
+            displayDate: null,
 
-        displayMileage: function () {
+            figureDisplayMileage: function () {
                 result = 0;
 
                 if (this.milesFloat != null && this.gallonsFloat != null) {
                     result = this.milesFloat / this.gallonsFloat;
+                    result = result.toFixed(2)
+                }
+
+                return result;
+            },
+
+            figureDisplayDate: function () {
+                result = "";
+
+                if (this.dateLong != null) {
+                    var dateObj = new Date(this.dateLong);
+                    var options = {
+                        month: "short",
+                        day: "numeric"
+                    };
+                    result = dateObj.toLocaleDateString("en-US", options);
                 }
 
                 return result;
--- src/ui-winjs/view/home/home.js
+++ src/ui-winjs/view/home/home.js
@@ -8,8 +8,15 @@
             bindingReady: function () {
                 var that = this;
                 var mpgDisplay = document.getElementById("mpgDisplay");
-                var fillup = new Fillup.Fillup();
-                WinJS.Binding.processAll(mpgDisplay, fillup);
+                var fillup = new Fillup.Fillup({
+                    carId: 1234,
+                    date: 123456789,
+                    miles: 1834.98,
+                    gallons: 43.6,
+                    pricePerGallon: 2.99,
+                    totalCost: 124.56
+                });
+                WinJS.Binding.processAll(homepageLatest, fillup);
             },
 
             myClickHandler: function () {
